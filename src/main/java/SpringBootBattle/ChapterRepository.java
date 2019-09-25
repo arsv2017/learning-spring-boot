1959e121fd14a24c9ea32637b39691b53cd26ce9
795222 <795222@cognizant.com>
2019-09-25 17:16:54 +0300

    firstCommit

---
+++ src/main/java/SpringBootBattle/ChapterRepository.java
@@ -0,0 +1,9 @@
+package SpringBootBattle;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface ChapterRepository extends ReactiveCrudRepository<Chapter, String> {
+
+
+
+}
